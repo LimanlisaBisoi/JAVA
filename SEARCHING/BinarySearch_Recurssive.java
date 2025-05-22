66c2c19999f0e9a3e929c0265c3f05dcc9eac5e8
Limanlisa Bisoi <150414261+LimanlisaBisoi@users.noreply.github.com>
2025-05-22 20:42:27 +0530

    Add files via upload

---
+++ SEARCHING/BinarySearch_Recurssive.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class BinarySearch_Recurssive {
+    public static void main(String[] args) 
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter the size of array and elements :");
+        int n = obj.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = obj.nextInt();
+         System.out.print("Enter search element :");
+        int key = obj.nextInt();
+        System.out.println(" binary search recursive Element present in the array : "+Search(arr,key,0,n-1));
+    }
+    public static boolean Search(int[] a,int x,int l,int r)
+    {
+        if(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(a[mid]==x)
+                return true;
+            if (a[mid]>x)
+                return Search(a,x,l,mid-1);
+            if (a[mid]<x)
+                return Search(a,x,mid+1,r);      
+        
+        }
+        return false;
+    }
+}
+
+/*                     OTHERWISE.........................
+public class q4 {
+    public static void main(String[] args) {
+        int[] arr = {10,75,5,47,67,34};
+        int key = 5;
+        int index = binarySearch(arr, key);
+        if (index == -1) {
+            System.out.println("Element not found in the array.");
+        } else {
+            System.out.println("Element found at index " + index + ".");
+        }
+    }
+
+    public static int binarySearch(int[] arr, int key) {
+        return binarySearchHelper(arr, key, 0, arr.length - 1);
+    }
+
+    public static int binarySearchHelper(int[] arr, int key, int a, int b) {
+        if (a > b) {
+            return -1;
+        }
+        int mid = (a + b) / 2;
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] < key) {
+            return binarySearchHelper(arr, key, mid + 1, b);
+        } else {
+            return binarySearchHelper(arr, key, a, mid - 1);
+        }
+    }
+}
+*/
---
+++ SEARCHING/Linear_Search.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Linear_Search {
+    public static void main(String[] args) 
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter the size of array and elements :");
+        int n = obj.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = obj.nextInt();
+        boolean found=false;
+         System.out.print("Enter search element :");
+        int key = obj.nextInt();
+        for (int i = 0; i < n; i++)
+            if(arr[i]== key)
+            {
+                found=true;
+                break;
+            }
+        System.out.println("linear search Element present in the array : "+found);
+    }
+}
+
+/*                        OTHERWISE....................
+public class q1{
+    public  static int linearSearch(int arr[], int n, int x) {
+       for (int i = 0; i < n; i++) {
+           if (arr[i] == x)
+               return i;
+       }
+       return -1;
+   }
+
+     public static void main(String[] args)  {
+       int[] arr = { 15,87,34,5,67,10 };
+       int n = arr.length;       
+       int x = 5;
+
+       int index = linearSearch(arr, n, x);
+       if (index == -1)
+           System.out.println("Element is not present in the array");
+       else
+           System.out.println("Element found at position " + index+" in the array.");
+   }
+}
+*/
