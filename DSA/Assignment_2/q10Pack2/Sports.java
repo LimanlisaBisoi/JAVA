accc9c11d4a81bd4d901493e369bb1687a6c1b2e
Limanlisa Bisoi <150414261+LimanlisaBisoi@users.noreply.github.com>
2025-05-22 10:42:00 +0530

    Add files via upload

---
+++ DSA/Assignment_2/q10Pack2/Sports.java
@@ -0,0 +1,12 @@
+//Create sport.java under q10pack2 and create sport interface in that package
+package q10pack2;
+
+import java.util.Scanner;
+
+public interface Sports {
+	Scanner sc = new Scanner(System.in);
+	int score1 = sc.nextInt();
+	int score2 = sc.nextInt();
+//	int score1 = 10;
+//	int score2= 10;
+}
